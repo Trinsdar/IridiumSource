b4a0b71ba86d848763a4ca24b7acd733d37021fb
Trinsdar <westbound1715@gmail.com>
2018-09-29 09:10:46 -0400

    Finished up the config.

--- src/main/java/com/lavx64/iridiumsource/Config.java
+++ src/main/java/com/lavx64/iridiumsource/Config.java
@@ -8,9 +8,9 @@ import net.minecraftforge.common.config.Configuration;
 public class Config {
     private static final String CATEGORY_GENERAL = "general";
 
-    public static int netherGenChance = 10;
-    public static int endGenChance= 15;
-    public static int overworldGenChance = 10;
+    public static int netherGenChance = 0;
+    public static int endGenChance= 0;
+    public static int overworldGenChance = 6;
     public static int netherMaxHeight = 256;
     public static int netherMinHeight = 0;
     public static int endMaxHeight = 256;
@@ -34,10 +34,11 @@ public class Config {
 
     private static void initGeneralConfig(Configuration cfg) {
         cfg.addCustomCategoryComment(CATEGORY_GENERAL, "General configuration");
+        cfg.addCustomCategoryComment(CATEGORY_GENERAL, "Recommended to set the gen chances to 0 and use another mod for generation if you want more control.");
         // cfg.getBoolean() will get the value in the config if it is already specified there. If not it will create the value.
-        netherGenChance = cfg.getInt("netherGenChance", CATEGORY_GENERAL, netherGenChance, 0, 300, "Set this to whatever value you want");
-        endGenChance = cfg.getInt("endGenChance", CATEGORY_GENERAL, endGenChance, 0, 300, "Set this to whatever value you want");
-        overworldGenChance = cfg.getInt("overworldGenChance", CATEGORY_GENERAL, overworldGenChance, 0, 300,  "Set this to whatever value you want");
+        netherGenChance = cfg.getInt("netherGenChance", CATEGORY_GENERAL, netherGenChance, 0, 500, "This determines the frequency of the ore. Set to 0 to disable.");
+        endGenChance = cfg.getInt("endGenChance", CATEGORY_GENERAL, endGenChance, 0, 500, "This determines the frequency of the ore. Set to 0 to disable.");
+        overworldGenChance = cfg.getInt("overworldGenChance", CATEGORY_GENERAL, overworldGenChance, 0, 500,  "This determines the frequency of the ore. Set to 0 to disable.");
         netherMaxHeight = cfg.getInt("netherMaxHeight", CATEGORY_GENERAL, netherMaxHeight, 1, 256,  "This determines the max height to generate the ore at.");
         endMaxHeight = cfg.getInt("endMaxHeight", CATEGORY_GENERAL, endMaxHeight, 1, 256,  "This determines the max height to generate the ore at.");
         overworldMaxHeight = cfg.getInt("overworldMaxHeight", CATEGORY_GENERAL, overworldMaxHeight, 1, 256, "This determines the max height to generate the ore at.");
